3ee6d6e205d0aceeaad00f5536d509e621a7181d
watchmakercomeback <ingecarrasquilla@gmail.com>
2022-06-21 11:53:11 -0500

    init-version-dos

--- KI-398695.js
+++ KI-398695.js
@@ -1,88 +1,23 @@
 $(document).ready(function () {
-  addSeachObserver();
-  /*const pkpmodalSearch = document.querySelector("#pkpmodal-search");
-  const input = pkpmodalSearch.querySelector("input");
-
-  input.addEventListener("change", (event) => {
-    console.log("change", event.target.value);
-    setTimeout(() => {
-      getOrderForm();
-    }, 100);
-  });*/
+  addModalObserver();
 });
-
-function getOrderForm() {
-  vtexjs.checkout.getOrderForm().then((orderForm) => {
-    getPupDistance(orderForm);
-  });
-}
-
-function getPupDistance(orderForm) {
-  const logisticsInfoSlas = orderForm.shippingData.logisticsInfo
-    .map((logisticInfo) => {
-      return logisticInfo.slas;
-    })
-    .flat();
-
-  const slasPUP = logisticsInfoSlas.filter((sla) => {
-    return sla.deliveryChannel === "pickup-in-point";
-  });
-  console.log("slasPUP", slasPUP);
-
-  const slas = [];
-  slasPUP.forEach((sla) => {
-    if (!slas.some((s) => s.id === sla.id)) {
-      slas.push({
-        id: sla.id,
-        value: sla,
-      });
-    }
-  });
-
-  console.log("slas", slas);
-
-  slas.forEach((sla) => {
-    const { pickupDistance } = sla.value;
-    console.log("pickupDistance", pickupDistance);
-    updatePupElements(sla.id, sla.value.pickupDistance);
-  });
-  console.log("getPupDistance end");
-}
-
-function updatePupElements(id, pickupDistance) {
-  //validar que exista el elemento
-  const parseId = id.replace(" ", "-").replace("(", "").replace(")", "");
-  const pkpmodalPointsList = document.querySelector(".pkpmodal-points-list");
-  const element = pkpmodalPointsList.querySelector("#" + parseId);
-  const distanceElement = element.querySelector(
-    ".pkpmodal-pickup-point-distance"
-  );
-  console.log("distanceElement", distanceElement);
-  const actualDistance = distanceElement.textContent;
-  console.log("actualDistance", actualDistance);
-
-  const unit = actualDistance.split(" ")[1];
-  console.log("pickupDistance", pickupDistance);
-  const parsePickupDistance = parseFloat(pickupDistance)
-    .toFixed(1)
-    .replace(".", ",");
-  const newDistance = parsePickupDistance + " " + unit;
-  console.log("newDistance", newDistance);
-  distanceElement.textContent = newDistance;
-}
-
 function addModalObserver() {
+  let open = false;
   const observerPupModal = new MutationObserver((mutations, obsPM) => {
-    const pupModal = document.querySelector("#pup-modal");
+    const pupModal = document.querySelector(".pkpmodal");
 
     if (document.contains(pupModal)) {
-      console.log('se abrio la modal')
-      addSeachObserver();
-
-      obsPM.disconnect();
-
-      return;
+      if (!open){
+          addSeachObserver();
+          open = !open
+          //obsPM.disconnect();
+          return;
+      }
     }
+    else{
+        open = false
+    }
+
   });
 
   observerPupModal.observe(document, {
@@ -92,16 +27,13 @@ function addModalObserver() {
 }
 function addSeachObserver() {
   const observerSearch = new MutationObserver((mutations, obsS) => {
-    const pkpmodalSearch = document.querySelector("#pkpmodal-search");
-
+    const pkpmodalSearch = document.querySelector("#pkpmodal-search"); 
     if (document.contains(pkpmodalSearch)) {
       const input = pkpmodalSearch.querySelector("input");
-
       input.addEventListener("change", (event) => {
-        console.log("change", event.target.value);
         setTimeout(() => {
           getOrderForm();
-        }, 100);
+        }, 500);
       });
 
       obsS.disconnect();
@@ -114,10 +46,76 @@ function addSeachObserver() {
   });
 }
 
-/*
 
-Br. Metropolis, Barrios Unidos, Bogotá, Colombia -> 0,4
+function getOrderForm() {
+vtexjs.checkout.getOrderForm().then((orderForm) => {
+  getPupDistance(orderForm);
+});
+}
 
-Acapulco, Engativá, Bogotá, Colombia -> 1,6
+function getPupDistance(orderForm) {
+const logisticsInfoSlas = orderForm.shippingData.logisticsInfo
+  .map((logisticInfo) => {
+    return logisticInfo.slas;
+  })
+  .flat();
 
-*/
+const slasPUP = logisticsInfoSlas.filter((sla) => {
+  return sla.deliveryChannel === "pickup-in-point";
+});
+const slas = [];
+slasPUP.forEach((sla) => {
+  if (!slas.some((s) => s.id === sla.id)) {
+    slas.push({
+      id: sla.id,
+      value: sla,
+    });
+  }
+});
+slas.forEach((sla) => {
+  const { pickupDistance } = sla.value;
+  addSectionObserver(sla.id, sla.value.pickupDistance);
+});
+}
+
+function updatePupElements(id, pickupDistance) {
+  //validar que exista el elemento
+  const parseId = id.replace(" ", "-").replace("(", "").replace(")", "");
+  const pkpmodalPointsList = document.querySelector(".pkpmodal-points-list");
+  const element = pkpmodalPointsList.querySelector("#" + parseId);
+  const distanceElement = element.querySelector(
+      ".pkpmodal-pickup-point-distance"
+  );
+  const actualDistance = distanceElement.textContent;
+  const unit = actualDistance.split(" ")[1];
+  const parsePickupDistance = parseFloat(pickupDistance)
+      .toFixed(1)
+      .replace(".", ",");
+  const newDistance = parsePickupDistance + " " + unit;
+  distanceElement.textContent = newDistance;
+}
+
+function addSectionObserver(id, pickupDistance) {
+  let open = false;
+  const observerSection = new MutationObserver((mutations, obsPM) => {
+  const pickupPointDistance = document.querySelector(".pkpmodal-pickup-point-distance");
+
+  if (document.contains(pickupPointDistance)) {
+      if (!open){
+          updatePupElements(id, pickupDistance);
+          open = !open
+          //obsPM.disconnect();
+          return;
+      }
+  }
+  else{
+      open = false
+  }
+
+  });
+
+  observerSection.observe(document, {
+  childList: true,
+  subtree: true,
+  });
+}
